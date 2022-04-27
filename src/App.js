9460a43631c5bbca1da3d6ab0508e5a980037304
Damian526 <58115973+Damian526@users.noreply.github.com>
2022-04-26 21:02:14 -0400

    v0001

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Welcome to my project page :)
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
